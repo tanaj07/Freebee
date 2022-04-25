73aff5b318880d3f7db17371bc4ebb5eea5d1178
Tanaj_khanuja <tanaj.deep07@gmail.com>
2022-04-26 01:11:54 +0530

    userauth

--- main/views.py
+++ main/views.py
@@ -7,6 +7,17 @@ def index(request):
     return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/index.html')
     
 def signup(request):
+    if request.method == "POST":
+     fname = request.POST['fname']
+     lname = request.POST['lname']
+     email = request.POST['email']
+     pass1 = request.POST['pass1']
+     pass2 = request.POST['pass2']
+     
+    
+
+
+
     context = {}
     return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/Signup.html', context)
 
