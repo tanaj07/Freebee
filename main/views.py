247a5156ac2981f928f92123e0bb36c3372f8984
Tanaj_khanuja <tanaj.deep07@gmail.com>
2022-04-28 18:47:42 +0530

    navbar

--- main/views.py
+++ main/views.py
@@ -10,18 +10,6 @@ def index(request):
     return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/index.html')
     
 def signup(request):
-    if request.method == "POST":
-        
-    
-
-     fname = request.POST['fname']
-     lname = request.POST['lname']
-     email = request.POST['email']
-     pass1 = request.POST['pass1']
-     pass2 = request.POST['pass2']
-
-
-
     context = {}
     return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/Signup.html', context)
 
@@ -37,28 +25,36 @@ def market(request):
     return render(request,'/Users/tanajkhanuja/Desktop/bee/main/templates/main/marketplace.html', context)
 
 def Graphicsanddesign(request):
-    return render(request,'/Users/tanajkhanuja/Desktop/bee/main/templates/main/Graphicsanddesign.html')
+    context = {}
+    return render(request,'/Users/tanajkhanuja/Desktop/bee/main/templates/main/Graphicsanddesign.html' , context)
 
 def Business(request):
-    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/business.html')
+    context = {}
+    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/business.html', context)
 
 def Data(request):
-    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/data.html')
+    context = {}
+    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/data.html', context)
 
 def Marketing(request):
-    return render(request , '/Users/tanajkhanuja/Desktop/bee/main/templates/main/marketing.html')
+    context = {}
+    return render(request , '/Users/tanajkhanuja/Desktop/bee/main/templates/main/marketing.html', context)
 
 def Musicandaudio(request):
-    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/Musicandaudio.html')
+    context = {}
+    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/Musicandaudio.html', context)
 
 def Programming(request):
-    return render(request , '/Users/tanajkhanuja/Desktop/bee/main/templates/main/Programming.html')
+    context = {}
+    return render(request , '/Users/tanajkhanuja/Desktop/bee/main/templates/main/Programming.html', context)
 
 def video(request):
-    return render(request , '/Users/tanajkhanuja/Desktop/bee/main/templates/main/video.html' )
+    context = {}
+    return render(request , '/Users/tanajkhanuja/Desktop/bee/main/templates/main/video.html' , context)
 
 def Writing(request):
-    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/writing.html')
+    context = {}
+    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/writing.html', context)
 
 def Pricing(request):
-    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/pricing.html')
+    return render(request, '/Users/tanajkhanuja/Desktop/bee/main/templates/main/pricing.html', )
